0ac19e36d7c8cdfb9a9ed276d0bffdc4b0baa0b6
Rhushabh <rathodrhushabh@gmail.com>
2024-12-05 11:11:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -12,5 +14,5 @@ export default {
       colors: {},
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
